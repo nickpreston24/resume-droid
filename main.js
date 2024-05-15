06edc93f04b88be9bd941da5aa16513b2a1e20ab
mikepreston17 <michael.n.preston@gmail.com>
2024-05-15 01:15:27 -0500

    ++ quilljs editor

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
-import './dist/style.css'
+// import './dist/style.css'
 import Alpine from "alpinejs";
+import persist from "@alpinejs/persist";
 
 window.Alpine = Alpine;
+Alpine.plugin(persist);
 Alpine.start();
+
+Alpine.store("darkMode", {
+  on: Alpine.$persist(true).as("darkMode_on"),
+});
+
+// from: https://github.com/quilljs/webpack-example/blob/main/src/index.html
+import "quill/dist/quill.snow.css";
+
+import Quill from "quill/core";
+
+import Toolbar from "quill/modules/toolbar";
+import Snow from "quill/themes/snow";
+
+import Bold from "quill/formats/bold";
+import Italic from "quill/formats/italic";
+import Header from "quill/formats/header";
+
+Quill.register({
+  "modules/toolbar": Toolbar,
+  "themes/snow": Snow,
+  "formats/bold": Bold,
+  "formats/italic": Italic,
+  "formats/header": Header,
+});
+
+new Quill("#editor", {
+  theme: "snow",
+});
+
+export default Quill;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // const plugin = require('tailwindcss/plugin')
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
@@ -26,15 +26,15 @@ module.exports = {
         "6xl": "4rem",
         "7xl": "5rem",
       },
-      animation: {
-        //   'spin-slow': 'spin 3s linear infinite',
-        //   wiggle: 'wiggle 1s ease-in-out infinite'
-      },
-      transitionDuration: {
-        0: "0ms",
-        2000: "2000ms",
-        3000: "3000ms",
-      },
+      // animation: {
+      //   //   'spin-slow': 'spin 3s linear infinite',
+      //   //   wiggle: 'wiggle 1s ease-in-out infinite'
+      // },
+      // transitionDuration: {
+      //   0: "0ms",
+      //   2000: "2000ms",
+      //   3000: "3000ms",
+      // },
     },
 
     screens: {
@@ -287,31 +287,14 @@ module.exports = {
     },
   },
   plugins: [require("daisyui")],
-  daisyui: {
-    themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
-  content: [
-    "./Pages/**/*.{cshtml}",
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  // daisyui: {
+  //   themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+  //   darkTheme: "dark", // name of one of the included themes for dark mode
+  //   base: true, // applies background color and foreground color for root element by default
+  //   styled: true, // include daisyUI colors and design decisions for all components
+  //   utils: true, // adds responsive and modifier utility classes
+  //   prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+  //   logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  //   themeRoot: ":root", // The element that receives theme color CSS variables
+  // },
 };
-
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html", // don't forget to add other files and directories
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: ["daisyui"],
-// };
